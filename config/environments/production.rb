1260ea76de6df21dad2feac9203b77eb58e54326
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-11-20 15:13:45 +0100

    production changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,8 +115,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.deface.enalbed = false
 
-  config.assets.enabled = true
-  config.assets.prefix = "/assets"
 end
