15807fb606d50537c6efb4e2fade515eb8de71a8
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-11-20 16:25:49 +0100

    rmagick gem added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
