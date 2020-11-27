20d4e05db3958e179bec7de4d8506323b512e76e
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-11-27 18:39:29 +0100

    Added a custom action to homecontroller 2

--- app/controllers/rails_online_store/spree/home_controller_decorator.rb
+++ app/controllers/rails_online_store/spree/home_controller_decorator.rb
@@ -12,24 +12,8 @@
 
 
 
-# Spree::HomeController.class_eval do
-#   # before_action :check_locale, only: :index
-#       def self.prepended(base)
-#         base.helper 'spree/products'
-#       end
-#     def index
-#         product_1 = ::Spree::Product.find_by!(name: 'Einkaufskorb Weide hell, oval, Henkel, 40 x 26 x 25/42')
-#         product_2 = ::Spree::Product.find_by!(name: 'Einkaufskorb Weide hell, oval, Henkel, 40 x 26 x 25/42')
-#         product_3 = ::Spree::Product.find_by!(name: 'Einkaufskorb Weide hell, oval, Henkel, 40 x 26 x 25/42')
-#         product_4 = ::Spree::Product.find_by!(name: 'Einkaufskorb Weide hell, oval, Henkel, 40 x 26 x 25/42')
-#         @homepage_products = [product_1, product_2, product_3, product_4]
-#       end
-# end
-
-
-module RailsOnlineStore
-  module Spree
-    module HomeControllerDecorator
+Spree::HomeController.class_eval do
+  # before_action :check_locale, only: :index
       def self.prepended(base)
         base.helper 'spree/products'
       end
@@ -40,7 +24,23 @@ module RailsOnlineStore
         product_4 = ::Spree::Product.find_by!(name: 'Einkaufskorb Weide hell, oval, Henkel, 40 x 26 x 25/42')
         @homepage_products = [product_1, product_2, product_3, product_4]
       end
-    end
-  end
 end
-::Spree::HomeController.prepend RailsOnlineStore::Spree::HomeControllerDecorator if ::Spree::HomeController.included_modules.exclude?(RailsOnlineStore::Spree::HomeControllerDecorator)
+
+
+# module RailsOnlineStore
+#   module Spree
+#     module HomeControllerDecorator
+#       def self.prepended(base)
+#         base.helper 'spree/products'
+#       end
+#     def index
+#         product_1 = ::Spree::Product.find_by!(name: 'Einkaufskorb Weide hell, oval, Henkel, 40 x 26 x 25/42')
+#         product_2 = ::Spree::Product.find_by!(name: 'Einkaufskorb Weide hell, oval, Henkel, 40 x 26 x 25/42')
+#         product_3 = ::Spree::Product.find_by!(name: 'Einkaufskorb Weide hell, oval, Henkel, 40 x 26 x 25/42')
+#         product_4 = ::Spree::Product.find_by!(name: 'Einkaufskorb Weide hell, oval, Henkel, 40 x 26 x 25/42')
+#         @homepage_products = [product_1, product_2, product_3, product_4]
+#       end
+#     end
+#   end
+# end
+# ::Spree::HomeController.prepend RailsOnlineStore::Spree::HomeControllerDecorator if ::Spree::HomeController.included_modules.exclude?(RailsOnlineStore::Spree::HomeControllerDecorator)
